5bdda2f737fc29fc0d9f06767e9f3ad25df6111d
Mohd Amaan <mohd.amaan8642@gmail.com>
2026-03-16 14:31:01 +0530

    feat: add spring assignment 1.1

---
+++ 16-03-26/Basic_Spring-1.1/App.java
@@ -0,0 +1,19 @@
+package com.demo.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.demo.springcore.beans.Employee;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("springconf.xml");
+        Employee emp = (Employee) context.getBean("emp");
+        System.out.println("Employee Details:");
+        System.out.println("-------------------------");
+        System.out.println(emp);
+    }
+}
