070f860363ca9dc45b013802cafe2927702fd4c7
Mohd Amaan <mohd.amaan8642@gmail.com>
2026-03-30 09:38:21 +0530

    feat: add Trainer API

---
+++ 18-03-26/Trainer_API/TraineeService.java
@@ -0,0 +1,50 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Trainee;
+import com.example.demo.repo.TraineeRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TraineeService {
+
+    @Autowired
+    private TraineeRepository traineeRepo;
+
+    public List<Trainee> getAllTrainees() {
+        return traineeRepo.findAll();
+    }
+
+    public Trainee getTraineeById(int id) {
+        Optional<Trainee> trainee = traineeRepo.findById(id);
+        return trainee.orElse(null);
+    }
+    
+    public Trainee insertTrainee(Trainee trainee) {
+        return traineeRepo.save(trainee);
+    }
+
+    public Trainee updateTrainee(int id, Trainee updatedTrainee) {
+        Optional<Trainee> existing = traineeRepo.findById(id);
+        if (existing.isPresent()) {
+            Trainee trainee = existing.get();
+            trainee.setTraineeName(updatedTrainee.getTraineeName());
+            trainee.setTraineeDomain(updatedTrainee.getTraineeDomain());
+            trainee.setTraineeLocation(updatedTrainee.getTraineeLocation());
+            return traineeRepo.save(trainee);
+        }
+        return null;
+    }
+
+    public String deleteTrainee(int id) {
+        if (traineeRepo.existsById(id)) {
+            traineeRepo.deleteById(id);
+            return "Trainee with ID " + id + " deleted successfully.";
+        }
+        return "Trainee not found.";
+    }
+}
